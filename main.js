7a70bc3b3b58b4f77896857320f875e70318a354
KR <78811949+KRONIX31@users.noreply.github.com>
2023-02-10 00:11:25 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const videoBackground = document.querySelector('video')
+const deployButton = document.querySelector('.deploy_button')
+const headerSubMenu = document.querySelector('.header_submenu')
 videoBackground.playbackRate = 0.71
 
 if (sessionStorage.getItem("is_reloaded")) {
@@ -26,3 +28,28 @@ const observer = new IntersectionObserver((entries) => {
 document.querySelectorAll('section').forEach((section)=>{
     observer.observe(section)
 })
+
+deployButton.addEventListener('click', ()=>{
+    if(deployButton.classList.contains('nav_active_hash')){
+        deployButton.classList.remove('nav_active_hash')
+        headerSubMenu.classList.remove('header_submenu_active')
+        setTimeout(()=>{
+            headerSubMenu.style.display = 'none'
+        }, 250)
+    } else{
+        deployButton.classList.add('nav_active_hash')
+        headerSubMenu.style.display = 'block'
+        setTimeout(()=>{
+            headerSubMenu.classList.add('header_submenu_active')
+        }, 50)
+    }
+})
+window.onclick = function(e) {
+    if (!e.target.matches('.deploy_button')) {
+        if (headerSubMenu.classList.contains('header_submenu_active')){
+            console.log('remove')
+            headerSubMenu.classList.remove('header_submenu_active')
+            deployButton.classList.remove('nav_active_hash')
+        }
+    }
+}
